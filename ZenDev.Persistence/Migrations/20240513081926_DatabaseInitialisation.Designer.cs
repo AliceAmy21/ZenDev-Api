441d8d08c7967b3c00e9f4c4c3f13d5f1257a0f7
Aaliya Chichwadia <aaliya.chichwadia@dariel.co.za>
2024-05-13 10:31:16 +0200

    database updates

--- ZenDev.Persistence/Migrations/20240513081926_DatabaseInitialisation.Designer.cs
+++ ZenDev.Persistence/Migrations/20240513081926_DatabaseInitialisation.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ZenDev.Persistence;
 
@@ -11,9 +12,11 @@ using ZenDev.Persistence;
 namespace ZenDev.Persistence.Migrations
 {
     [DbContext(typeof(ZenDevDbContext))]
-    partial class ZenDevDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240513081926_DatabaseInitialisation")]
+    partial class DatabaseInitialisation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
