2a455722b7d3de20f93e2b0db3ece2d4fb4977c6
Amber <amber-leigh.lezar@dariel.co.za>
2024-05-27 14:59:26 +0200

    Create methods in group service

--- ZenDev.BusinessLogic/Services/GroupService.cs
+++ ZenDev.BusinessLogic/Services/GroupService.cs
@@ -1,8 +1,23 @@
 ﻿using ZenDev.BusinessLogic.Services.Interfaces;
+using ZenDev.Persistence.Entities;
 
 namespace ZenDev.BusinessLogic.Services
 {
     public class GroupService : IGroupService
     {
+        public async Task<List<GroupEntity>> getAllGroupsAsync(long userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<GroupEntity>> getAvailableGroupsAsync(long userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<GroupEntity?> getGroupByIdAsync(long groupId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- ZenDev.BusinessLogic/Services/Interfaces/IGroupService.cs
+++ ZenDev.BusinessLogic/Services/Interfaces/IGroupService.cs
@@ -1,6 +1,13 @@
-﻿namespace ZenDev.BusinessLogic.Services.Interfaces
+﻿using ZenDev.Persistence.Entities;
+
+namespace ZenDev.BusinessLogic.Services.Interfaces
 {
     public interface IGroupService
     {
+        public Task<List<GroupEntity>> getAllGroupsAsync(long userId);
+
+        public Task<List<GroupEntity>> getAvailableGroupsAsync(long userId);
+
+        public Task<GroupEntity?> getGroupByIdAsync(long groupId);
     }
 }
