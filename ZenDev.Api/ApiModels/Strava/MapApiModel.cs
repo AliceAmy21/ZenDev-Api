b36640e48c7d9895a40baaa1e6749b0a7a085f5b
Amber <amber-leigh.lezar@dariel.co.za>
2024-06-12 11:36:15 +0200

    Create strava objects

---
+++ ZenDev.Api/ApiModels/Strava/MapApiModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ZenDev.Api.ApiModels.Strava
+{
+    public class MapApiModel
+    {
+        public int Id { get; set; }
+        public string summaryPolyline { get; set; } = string.Empty;
+        public int resourceState { get; set; }
+    }
+}
