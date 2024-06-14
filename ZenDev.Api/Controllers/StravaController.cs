ec60f4e814ab4492103ea569e86cf057459db23e
Amber <amber-leigh.lezar@dariel.co.za>
2024-06-14 14:59:20 +0200

    Update controller to get points as well

--- ZenDev.Api/Controllers/StravaController.cs
+++ ZenDev.Api/Controllers/StravaController.cs
@@ -1,24 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Net.Http.Headers;
 using System.Text.Json;
 using ZenDev.Api.ApiModels.Strava;
+using ZenDev.BusinessLogic.Services.Interfaces;
+using ZenDev.Common.Models;
 
 namespace ZenDev.Api.Controllers
 {
-    public class StravaController
+    [ApiController]
+    public class StravaController: ControllerBase
     {
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IMapper _mapper;
         private readonly ILogger<StravaController> _logger;
+        private readonly IPointsService _pointsService;
 
-        public StravaController(IHttpClientFactory httpClientFactory, ILogger<StravaController> logger)
+        public StravaController(
+            IHttpClientFactory httpClientFactory,
+            IMapper mapper,
+            IPointsService pointsService,
+            ILogger<StravaController> logger
+            )
         {
             _httpClientFactory = httpClientFactory;
+            _mapper = mapper;
+            _pointsService = pointsService;
             _logger = logger;
         }
 
         [HttpPost(nameof(SyncStravaData))]
-        public async Task <ActionResult<ActivitySummaryApiModel>> SyncStravaData([FromBody] string accessToken)
+        public async Task <ActionResult<List<ActivitySummaryApiModel>>> SyncStravaData([FromBody] string accessToken)
         {
             var httpClient = _httpClientFactory.CreateClient("strava");
 
@@ -35,10 +48,20 @@ namespace ZenDev.Api.Controllers
 
                 var activities = JsonSerializer.Deserialize<List<ActivitySummaryResponse>>(stream);
                 _logger.LogInformation("Activities: {Activities}", activities);
+
+                var activitiesApiModel = _mapper.Map<List<ActivitySummaryApiModel>>(activities);
+                _logger.LogInformation("Mapped Activities: {MappedActivities}", activitiesApiModel);
+
+                var pointsModels = _mapper.Map<List<ActivityPointsApiModel>>(activitiesApiModel);
+
+                int points = _pointsService.CalculatePoints(pointsModels);
+                _logger.LogInformation("Points: {MappedPoints}", points);
+
+                return Ok(activitiesApiModel);
+
             }
 
-
-            return null;
+            return BadRequest();
         }
     }
 }
