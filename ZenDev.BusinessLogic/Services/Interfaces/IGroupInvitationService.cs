311f3203287c4179c269436ac65879e12f7cab16
Trevarn Govender <trevarn.govender@dariel.co.za>
2024-06-18 11:39:30 +0200

    Changed group invitations functions to handle a list

--- ZenDev.BusinessLogic/Services/Interfaces/IGroupInvitationService.cs
+++ ZenDev.BusinessLogic/Services/Interfaces/IGroupInvitationService.cs
@@ -6,7 +6,7 @@ namespace ZenDev.BusinessLogic.Services.Interfaces
 {
     public interface IGroupInvitationService
     {
-        public Task<GroupInvitationEntity> CreateGroupInvitationAsync(GroupInvitationEntity groupInvitation);
+        public Task<List<GroupInvitationEntity>> CreateGroupInvitationsAsync(List<GroupInvitationEntity> groupInvitations);
         public Task<List<GroupInvitationEntity?>> GetGroupInvitationsByUserIdAsync(long userId);
         public Task<List<UserInviteModel>> GetNonGroupMembers(long groupId);
         public Task<List<UserInviteModel>> GetAllUsersAsync(GroupInvitationQueryObject query);
